8f77625e6af34e723dbd037b3af0b3c0f75ab05a
AHuangjinhui <2524500884@qq.com>
2020-10-28 22:46:19 +0800

    Added an index page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html>
 	<head>
-		<title>DGPT4711 Lab 06</title>
+		<title>DGPT4711 Assignment1</title>
 	</head>
 	<body>
-		<h1>Travel Destinations</h1>
-		<p><a href="/travel">Show the destinations</a>.</p>
+		<h1>Chinese Comedians</h1>
+		<p><a href="/travel">Display character names</a>.</p>
 		<p><a href="/places">Show the places raw data</a></p>
 	</body>
 </html>
